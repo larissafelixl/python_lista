c03cef5dc4a875fa3a506487d2bed9a51668e3f8
larissafelixl <larissafelixpalhano@gmail.com>
2024-06-09 20:52:26 -0300

    commitando as questões com alterações.

--- 09_questao.py
+++ 09_questao.py
@@ -1,5 +1,5 @@
 n = 0
 while n <= 8000:
-    print(n)#(n, end=' ')colocaria todos os números na mesma linha caso p rofessor prefira na correçõa!
+    print(n)#(n, end=' ')colocaria todos os números na mesma linha caso p rofessor prefira na correçõa!!
     n += 10 
 
